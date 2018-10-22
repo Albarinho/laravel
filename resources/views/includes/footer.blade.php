b122db023739cd4a92d6d87c7f68587b20401145
albaro claros <ac484@njit.edu>
2018-10-22 14:59:28 -0400

    finish two page website

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="container"  class="font-weight-light">
+    <p>&copy; 2018-2019 NJIT &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
+    </p>
+</div>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+    <h1> Hello World, welcome to my about page</h1>
+
+@stop
